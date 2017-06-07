83aeacf56bcf3a3b7702cfdb81752ade5ee52bcd
engine77 <zaur.1kager@gmail.com>
2017-06-07 18:11:08 +0400

    catalog

---
+++ catalog.php
@@ -0,0 +1,179 @@
+<?php require_once('includes/header.php'); ?>
+
+<div class="header-slider">
+	<span class="header-title">На  природе <br/>вкуснее!</span><br/>
+	<span class="header-slogan">
+		товары для пикника<br/>
+		оптом от производителя
+	</span>
+
+	<div class="slider-wrap">
+		<div class="slider-container">
+			<div id="slider__actual">
+
+				<div class="item">
+					<div class="item-box">
+						<a href="#">
+							<div class="item-box-img">
+								<img src="img/p1.png" alt="">
+							</div>
+							<p>
+								Хозяйственные товары
+							</p>
+						</a>
+					</div>
+				</div>
+
+				<div class="item">
+					<div class="item-box">
+						<a href="#">
+							<div class="item-box-img">
+								<img src="img/p2.png" alt="">
+							</div>
+							<p>
+								Бумажно-гигиеническая продукция
+							</p>
+						</a>
+					</div>
+				</div>	
+
+				<div class="item">
+					<div class="item-box">
+						<a href="#">
+							<div class="item-box-img">
+								<img src="img/p3.png" alt="">
+							</div>
+							<p>
+								Бумажная продукция
+							</p>
+						</a>
+					</div>
+				</div>	
+
+				<div class="item">
+					<div class="item-box">
+						<a href="#">
+							<div class="item-box-img">
+								<img src="img/p4.png" alt="">
+							</div>
+							<p>
+								Профессиональная химия
+							</p>
+						</a>
+					</div>
+				</div>
+
+				<div class="item">
+					<div class="item-box">
+						<a href="#">
+							<div class="item-box-img">
+								<img src="img/p5.png" alt="">
+							</div>
+							<p>
+								Бумага и чековая лента
+							</p>
+						</a>
+					</div>
+				</div>	
+
+				<div class="item">
+					<div class="item-box">
+						<a href="#">
+							<div class="item-box-img">
+								<img src="img/p6.png" alt="">
+							</div>
+							<p>
+								Пленка упаковочная
+							</p>
+						</a>
+					</div>
+				</div>	
+
+				<div class="item">
+					<div class="item-box">
+						<a href="#">
+							<div class="item-box-img">
+								<img src="img/p7.png" alt="">
+							</div>
+							<p>
+								Одноразовая посуда
+							</p>
+						</a>
+					</div>
+				</div>	
+
+				<div class="item">
+					<div class="item-box">
+						<a href="#">
+							<div class="item-box-img">
+								<img src="img/p8.png" alt="">
+							</div>
+							<p>
+								Пакеты
+							</p>
+						</a>
+					</div>
+				</div>	
+
+				<div class="item">
+					<div class="item-box">
+						<a href="#">
+							<div class="item-box-img">
+								<img src="img/p9.png" alt="">
+							</div>
+							<p>
+								Одноразовая упаковка
+							</p>
+						</a>
+					</div>
+				</div>	
+
+				<div class="item">
+					<div class="item-box">
+						<a href="#">
+							<div class="item-box-img">
+								<img src="img/p10.png" alt="">
+							</div>
+							<p>
+								Бытовая химия
+							</p>
+						</a>
+					</div>
+				</div>								
+
+			</div>
+		</div>
+		<div class="slider-line"></div>
+	</div>
+
+</div>
+
+</div>
+
+<div class="about-company__wrap">
+	<div class="container">
+		<div class="row">
+			<h2>О компании</h2>
+			<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+				<div class="about-company">
+					<img class="img-responsive" src="img/about.jpg" alt="">
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+				<div class="about-company about-company__right">
+				<p>Торговая Компания «Паритет» с 2003 года осуществляет свою<br/> деятельность на рынке  расходных материалов и упаковки.
+					</p>
+					<p>
+						Мы работаем с ведущими производителями пластиковой<br/> продукции и упаковки.<br/>
+						Нашими клиентами являются кафе, рестораны, магазины, клининговые  компании, а также компании занимающиеся<br/> доставкой обедов.<br/>
+						- Одноразовая посуда: стаканы, чашки, тарелки, вилки, ложки, ножи, контейнеры, лотки 
+					</p>
+					<a class="but but_1 but_1__padding" href="#">Подробнее</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<?php require_once('includes/footer.php'); ?>
