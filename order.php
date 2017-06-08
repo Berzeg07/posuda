dd52fccda2cb158df516838234ab0b3a549e072c
Berzeg07 <berezg86@gmail.com>
2017-06-08 16:20:34 +0400

    31.05

--- order.php
+++ order.php
@@ -232,6 +232,191 @@
 			</div>
 		</div>
 	</div>
+
+	<div class="busket">
+	<h2 class="busket_caption">Состав заказа</h2>
+
+
+		<div class="busket_table">
+			<div class="busket_table__name">
+				<span class="busket__nameproduct">Наименование товара</span>
+				<div class="busket_table__img">
+					<img src="img/busket1.jpg" alt="">
+				</div>
+				<div class="busket_table__txtbox">
+					<h4>Сумка пластиковая ПЭ серый 157*467*520мм. </h4>
+					<span>Артикул: 413800в </span>
+					<span>Цвет: Серый</span>
+					<span>Материал: ПЭ</span>
+				</div>
+			</div>
+			<div class="busket_table__pricebox">
+				<span class="busket_table__caption">Цена</span>
+				<div class="busket_table__price">
+					<span>15 руб.</span>
+				</div>
+			</div>
+			<div class="busket_table__kolvo">
+				<span class="busket_table__caption">Количество</span>
+				<div class="switch__number">
+					<span class="switch-button">+</span>
+					<input class="price_area" type="text" value="1" disabled="disabled" id="quantity">  
+					<span class="switch-button">-</span>
+				</div>
+			</div>
+			<div class="busket_table__sort">
+				<span class="busket_table__caption">Фасовка</span>
+				<div class="busket_table__sorttxt">
+					<span>50шт/уп</span>
+					<span>200шт/кор</span>
+				</div>
+			</div>		
+			<div class="busket_table__mass">
+				<span class="busket_table__caption">Вес</span>
+				<div class="busket_table__masstxt">
+					<span>63 кг</span>
+				</div>
+			</div>
+			<div class="busket_table__priceall">
+				<span class="busket_table__caption">Итого</span>
+				<div class="busket_table__pricealltxt">
+					<span>586 руб.</span>
+				</div>
+			</div>
+			<div class="busket_table__delete">
+				<span class="busket_table__caption"></span>
+				<a href="#"></a>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+
+		<div class="busket_table">
+			<div class="busket_table__name">
+				<span class="busket__nameproduct">Наименование товара</span>
+				<div class="busket_table__img">
+					<img src="img/busket1.jpg" alt="">
+				</div>
+				<div class="busket_table__txtbox">
+					<h4>Сумка пластиковая ПЭ серый 157*467*520мм. </h4>
+					<span>Артикул: 413800в </span>
+					<span>Цвет: Серый</span>
+					<span>Материал: ПЭ</span>
+				</div>
+			</div>
+			<div class="busket_table__pricebox">
+				<span class="busket_table__caption">Цена</span>
+				<div class="busket_table__price">
+					<span>15 руб.</span>
+				</div>
+			</div>
+			<div class="busket_table__kolvo">
+				<span class="busket_table__caption">Количество</span>
+				<div class="switch__number">
+					<span class="switch-button">+</span>
+					<input class="price_area" type="text" value="1" disabled="disabled" id="quantity">  
+					<span class="switch-button">-</span>
+				</div>
+			</div>
+			<div class="busket_table__sort">
+				<span class="busket_table__caption">Фасовка</span>
+				<div class="busket_table__sorttxt">
+					<span>50шт/уп</span>
+					<span>200шт/кор</span>
+				</div>
+			</div>		
+			<div class="busket_table__mass">
+				<span class="busket_table__caption">Вес</span>
+				<div class="busket_table__masstxt">
+					<span>63 кг</span>
+				</div>
+			</div>
+			<div class="busket_table__priceall">
+				<span class="busket_table__caption">Итого</span>
+				<div class="busket_table__pricealltxt">
+					<span>586 руб.</span>
+				</div>
+			</div>
+			<div class="busket_table__delete">
+				<span class="busket_table__caption"></span>
+				<a href="#"></a>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+
+		<div class="busket_table">
+			<div class="busket_table__name">
+				<span class="busket__nameproduct">Наименование товара</span>
+				<div class="busket_table__img">
+					<img src="img/busket1.jpg" alt="">
+				</div>
+				<div class="busket_table__txtbox">
+					<h4>Сумка пластиковая ПЭ серый 157*467*520мм. </h4>
+					<span>Артикул: 413800в </span>
+					<span>Цвет: Серый</span>
+					<span>Материал: ПЭ</span>
+				</div>
+			</div>
+			<div class="busket_table__pricebox">
+				<span class="busket_table__caption">Цена</span>
+				<div class="busket_table__price">
+					<span>15 руб.</span>
+				</div>
+			</div>
+			<div class="busket_table__kolvo">
+				<span class="busket_table__caption">Количество</span>
+				<div class="switch__number">
+					<span class="switch-button">+</span>
+					<input class="price_area" type="text" value="1" disabled="disabled" id="quantity">  
+					<span class="switch-button">-</span>
+				</div>
+			</div>
+			<div class="busket_table__sort">
+				<span class="busket_table__caption">Фасовка</span>
+				<div class="busket_table__sorttxt">
+					<span>50шт/уп</span>
+					<span>200шт/кор</span>
+				</div>
+			</div>		
+			<div class="busket_table__mass">
+				<span class="busket_table__caption">Вес</span>
+				<div class="busket_table__masstxt">
+					<span>63 кг</span>
+				</div>
+			</div>
+			<div class="busket_table__priceall">
+				<span class="busket_table__caption">Итого</span>
+				<div class="busket_table__pricealltxt">
+					<span>586 руб.</span>
+				</div>
+			</div>
+			<div class="busket_table__delete">
+				<span class="busket_table__caption"></span>
+				<a href="#"></a>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+
+		<div class="busket__allsummbox">
+			<span class="clearbusket">Очистить корзину</span>
+			<div class="busket__tovar">
+				<h3 class="busket__tovar__cap"><b>3</b> товара на сумму</h3>
+				<span class="busket__tovar__summ">1758 руб.</span>
+				<span class="busket__tovar__mass">Общий вес; 287 кг</span>
+				<a class="but but_5" href="#">Оформить заказ</a>
+			</div>
+			<div class="clearfix"></div>
+		</div>	
+
+		<div class="busket__allsummbox busket__allsummbox2">
+			<div class="busket__tovar">
+				<span class="busket__tovar__cap">Итого:</span>
+				<span class="busket__tovar__summ">1758 руб.</span>
+				<span class="busket__tovar__mass">Общий вес; 287 кг</span>
+				<a class="but but_5" href="#">Подвердить заказ</a>
+			</div>
+			<div class="clearfix"></div>
+		</div>	
+	</div>
 </div>
 
 
